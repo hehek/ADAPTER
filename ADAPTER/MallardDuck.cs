57fb7510f54f3203cab6bf5d4ee3ce0a8c52c741
Anna Pavlovskaya <annvsann@gmail.com>
2019-04-01 00:50:02 +0300

    Iduck, duck

---
+++ ADAPTER/MallardDuck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADAPTER
+{
+    class MallardDuck: IDuck
+    {
+        public void quack()
+        {
+            Console.WriteLine("QUACK-QUACK-QUACK");
+        }
+
+        public void fly()
+        {
+            Console.WriteLine("I'm flying, man");
+        }
+    }
+}
